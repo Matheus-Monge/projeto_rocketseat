5f46d8196e7263ae235754870f400c76c02d23ec
Matheus Monge <matheus100_alcantara@hotmail.com>
2023-10-06 11:14:43 -0300

    change photo image and "alt"

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toggleMode() {
      if(html.classList.contains('light')) {
          // se tiver light mode, adicionar a imagem light
          img.setAttribute('src', './assets/avatar-light.png')
-         img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos escuro e jaqueta cinza, sem barba e fundo azul')
+         img.setAttribute("alt", "Foto de Matheus Monge sorrindo, usando óculos e camisa social, barba e fundo embaçado.")
      } else {
          // se tiver sem light mode, manter a imagem normal
          img.setAttribute('src', './assets/avatar.png')
